69341b8450f5cb34f0e2f072f34bbb261d85d970
MrMrRPG <67204447+MrMrRPG@users.noreply.github.com>
2021-07-21 11:40:18 -0700

    Add files via upload

---
+++ starter_code/api_keys.py
@@ -0,0 +1,9 @@
+# OpenWeatherMap API Key
+weather_api_key = "63bd86c4806561c4f6f018a2d5017513"
+
+# Google API Key
+<<<<<<< HEAD
+g_key = "AIzaSyDxuPoaDbir9TwQvRqnZsP3nTtEoj0olZ"
+=======
+g_key = "AIzaSyDxuPoaDbir9TwQvRqnZsP3nTtEoj0olZU"
+>>>>>>> 5d0830e6da0431dd1560bc9bfecc293a6e15058e
